ee10bbaa9135d3639de6099316ab9f810e4c1106
Ok-ss <kotliarchuk.pn@ucu.edu.ua>
2024-04-08 14:23:50 +0300

    can you get a loop

---
+++ Can you get a loop/can_you_get_a _loop.py
@@ -0,0 +1,23 @@
+class Node:
+    '''class to store info about nods'''
+    def __init__(self, data):
+        self.data = data
+        self.next = None 
+
+def loop_size(node):
+    if node.next:
+        node_1 = node
+        node_2 = node
+        node_1 = node_1.next
+        node_2 = node_2.next.next
+        strt_count = True
+        counter = 0
+        while node_1 != node_2 or strt_count:
+            if node_1 == node_2:
+                strt_count = False
+            node_1 = node_1.next
+            node_2 = node_2.next.next
+            if not strt_count:
+                counter += 1
+        return counter
+    raise Exception
